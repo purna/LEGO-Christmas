c8954cf62a7f524e9bbe025d2d163def9de25d82
Nigel Morris <nigel@pixelagent.co.uk>
2023-12-11 16:17:00 +0000

    v3

--- Assets/Gfx/Pref.cs
+++ Assets/Gfx/Pref.cs
@@ -1,13 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.LEGO.Game;
 
-public class Pref : MonoBehaviour
+namespace Unity.LEGO.UI
 {
-    public int levelnum;
-    void Start()
+    public class Pref : MonoBehaviour
     {
-        PlayerPrefs.SetInt("Level", levelnum);
-    }
+        public int levelnum;
 
+        bool m_GameOver;
+        bool m_Won;
+        int level;
+
+
+         void Start()
+        {
+            EventManager.AddListener<GameOverEvent>(OnGameOver);    
+        }
+
+         void Update()
+        {
+            if (m_GameOver)
+            {
+                // Save level.
+                if (m_Won){
+
+                    if (!PlayerPrefs.HasKey ("Level"))
+                    {
+                        PlayerPrefs.SetInt("Level", levelnum);
+                    } 
+                    else 
+                    {
+                        level = PlayerPrefs.GetInt("Level");
+
+                        if (level <= levelnum)
+                         {
+                            PlayerPrefs.SetInt("Level", levelnum);
+                         }
+                    
+                    }
+       
+
+                    
+                }
+                  
+            }
+        }
+
+        void OnGameOver(GameOverEvent evt)
+        {
+            if (!m_GameOver)
+            {
+                m_GameOver = true;
+                m_Won = evt.Win;
+            }
+        }
+
+        void OnDestroy()
+        {
+            EventManager.RemoveListener<GameOverEvent>(OnGameOver);
+        }
+
+    }
 }
